30b49f6f2b64fa0ebeb4aea3fb038f525acdb613
Mateusz Zurawski <mkzurawski94@gmail.com>
2020-03-23 14:56:05 +0100

    +tablice

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
 public class Main {
+
+
     public static void main (String [] args) {
-        System.out.println("hello world");
+//      TABLICA Z IMIENIEM ORAZ NAZWISKIEM
+
+//        String[] imieNazwisko = new String[3];
+//        imieNazwisko[0] = "Mateusz Zurawski";
+//        imieNazwisko[1] = "Martyna Wichert";
+//        imieNazwisko[2] = "Adam Nowak";
+//
+//
+//        int a;
+//        for(a=0 ; a <= imieNazwisko.length - 1 ; a++)
+//            {
+//                System.out.println(imieNazwisko[a]);
+//            }
+
+
+//      TABLICA Z WIEKIEM
+//        int[] wiek= {14, 24, 38};
+//        int b;
+//        for(b=0 ; b <= wiek.length - 1 ; b++)
+//        {
+//            System.out.println(wiek[b]);
+//        }
+//        System.out.println("pierwszy wyraz tablicy to" + wiek[0]);
+//        System.out.println("ostatni wyraz tablicy to " + wiek[wiek.length-1]);
+
+//      TABLICA WYPISUJACA LICZBY OD 0 DO X
+
+//        int[] liczby=new int[4];
+//        int c =0;
+//
+//        for(int d = 0; d <= liczby.length-1; d++)
+//        {
+//            liczby[c]=c;
+//            System.out.println("tab " + liczby[c]);
+//            c++;
+//        }
+
+
+
+
     }
-
-
 }
